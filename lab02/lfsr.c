1f8ac670c6380157995a6c2c1c309965402a7b08
git898475 <s64824910@gmail.com>
2026-02-09 19:49:20 +0800

    lab01 and lab02 finished!

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -4,7 +4,12 @@
 #include <string.h>
 #include "lfsr.h"
 
-void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+uint16_t get_bit(uint16_t reg,uint16_t n){
+    return (reg >> n) & 1;
 }
 
+void lfsr_calculate(uint16_t *reg) {
+    /* YOUR CODE HERE */
+    uint16_t shift_in = get_bit(*reg, 0) ^ get_bit(*reg, 2) ^ get_bit(*reg, 3) ^ get_bit(*reg, 5);
+    *reg = ((*reg) >> 1) | (shift_in << 15);
+}
